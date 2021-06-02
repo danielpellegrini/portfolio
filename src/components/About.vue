754416b7b4e37e0923a28442a249ad116e9fa331
danielpellegrini <danielpellegrini87@gmail.com>
2021-06-02 11:12:32 +0200

    initial commit

---
+++ src/components/About.vue
@@ -0,0 +1,10 @@
+ <template> 
+    <div>
+        <div class="holoContainerLayover"></div>
+        <div class="holoContainer">
+            <div class="holo-top">
+                <h2 class="text-center">About</h2>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="main-container">
+    <div class="layover">
+      <Navigation/>
+    </div> 
+
+    <carousel-3d :width="500" :height="700">
+      <slide :index="0">
+        <About/>
+      </slide>
+      <slide :index="1">
+        <Projects/>
+      </slide>
+      <slide :index="2">
+        <Contacts/>
+      </slide>
+    </carousel-3d>
+
+  </div>
+  
+</template>
+
+<script>
+import Navigation from './Navigation'
+import About from './About'
+import Projects from './Projects'
+import Contacts from './Contacts'
+import { Carousel3d, Slide } from 'vue-carousel-3d';
+export default {
+  name: 'Home',
+  components: {
+    Navigation,
+    About,
+    Projects,
+    Contacts,
+    Carousel3d,
+    Slide
+  },
+  props: {
+
+  }
+}
+</script>

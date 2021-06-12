6f86b2a57029d07ccea81d8d2683031f35f2a9ac
danielpellegrini <danielpellegrini87@gmail.com>
2021-06-12 16:45:45 +0200

    optimization test

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -2,18 +2,10 @@
     <div id="welcomeComponent">
 
         <div id="welcome-container-top" @click="hideWelcome" >
-            <div class="welcome"> 
-
-                <!-- <h1 class="text text-flicker-in-glow">
-                    <v-scrollin id="scrollName" class="d-inline" :speed="25" :misses="3">Hi,</v-scrollin> 
-                </h1>
-
-                <h1 class="text text-flicker-in-glow">
-                    <v-scrollin id="scrollName" class="d-inline" :speed="25" :misses="3">I am</v-scrollin> 
-                    <span id="name" class="text-flicker-in-glow2">
-                        <v-scrollin id="scrollName" class="d-inline" :speed="30" :misses="3">Daniel Pellegrini</v-scrollin>
-                    </span> 
-                </h1> -->
+            <div class="welcome">             
+                <div class="text1"></div>
+                <div class="text2"></div>            
+                <div class="text3"></div>            
 
             </div>
         </div>
@@ -30,13 +22,7 @@
         <div id="welcome-container-bottom" @click="hideWelcome">
             <div class="welcome">
                 
-                    <!-- <h1 class="text text-flicker-in-glow">
-                        <v-scrollin id="scrollName" class="d-inline" :speed="25" :misses="3">and I am a</v-scrollin>
-                                        
-                        <span id="job" class="text-flicker-in-glow2">
-                            <v-scrollin id="scrollJob" class="d-inline" :speed="30" :misses="4">Full Stack Web Dev</v-scrollin>
-                        </span>
-                    </h1> -->
+             
                 
             </div> 
 
@@ -63,6 +49,106 @@ export default {
             document.getElementById("navbar").className = "layover";
             document.getElementsByTagName("UL")[0].className = "nav justify-content-center";
         }    
+    },
+    mounted() {
+        // ——————————————————————————————————————————————————
+        // TextScramble
+        // ——————————————————————————————————————————————————
+
+        class TextScramble {
+          constructor(el) {
+            this.el = el
+            this.chars = '!<>-_\\/[]{}—=+*^?#________'
+            this.update = this.update.bind(this)
+          }
+          setText(newText) {
+            const oldText = this.el.innerText
+            const length = Math.max(oldText.length, newText.length)
+            const promise = new Promise((resolve) => this.resolve = resolve)
+            this.queue = []
+            for (let i = 0; i < length; i++) {
+              const from = oldText[i] || ''
+              const to = newText[i] || ''
+              const start = Math.floor(Math.random() * 80)
+              const end = start + Math.floor(Math.random() * 80)
+              this.queue.push({ from, to, start, end })
+            }
+            cancelAnimationFrame(this.frameRequest)
+            this.frame = 0
+            this.update()
+            return promise
+          }
+          update() {
+            let output = ''
+            let complete = 0
+            for (let i = 0, n = this.queue.length; i < n; i++) {
+              let { from, to, start, end, char } = this.queue[i]
+              if (this.frame >= end) {                
+                output += to
+              } else if (this.frame >= start) {
+                if (!char || Math.random() < 0.88) {
+                  char = this.randomChar()
+                  this.queue[i].char = char
+                }
+                output += `<span class="dud">${char}</span>`
+              } else {
+                output += from
+              }
+            }
+            this.el.innerHTML = output
+            if (complete === this.queue.length) {
+              this.resolve()
+            } else {
+              this.frameRequest = requestAnimationFrame(this.update)
+              this.frame++
+            }
+          }
+          randomChar() {
+            return this.chars[Math.floor(Math.random() * this.chars.length)]
+          }
+        }
+
+        // ——————————————————————————————————————————————————
+        // Example
+        // ——————————————————————————————————————————————————
+
+        let phrase1 = [
+          'Hi,'
+        ]
+        const phrase2 = [
+          'I am <span>Daniel Pellegrini</span>'
+        ]
+        const phrase3 = [
+          'and I am a <span>Full Stack Web Dev</span>'
+        ]
+
+        const el1 = document.querySelector('.text1')
+        const el2 = document.querySelector('.text2')
+        const el3 = document.querySelector('.text3')
+        const fx1 = new TextScramble(el1)
+        const fx2 = new TextScramble(el2)
+        const fx3 = new TextScramble(el3)
+
+        let counter1 = 0
+        let counter2 = 0
+        let counter3 = 0
+        const next = () => {
+          fx1.setText(phrase1[counter1]).then(() => {
+            setTimeout(next, 80)
+          })
+          fx2.setText(phrase2[counter2]).then(() => {
+            setTimeout(next, 200)
+          })
+          fx3.setText(phrase3[counter3]).then(() => {
+            setTimeout(next, 5000)
+          })
+          counter1 = (counter1 + 1) % phrase1.length
+          counter2 = (counter2 + 1) % phrase2.length
+          counter3 = (counter3 + 1) % phrase3.length
+        }
+
+        next()
+
     }
 }
 </script>

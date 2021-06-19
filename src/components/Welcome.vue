48a43370baf1bef2254b93d1fcf664df3c26c5da
danielpellegrini <danielpellegrini87@gmail.com>
2021-06-19 14:07:15 +0200

    dist updated

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -119,8 +119,8 @@ export default {
             for (let i = 0; i < length; i++) {
               const from = oldText[i] || ''
               const to = newText[i] || ''
-              const start = Math.floor(Math.random() * 256)
-              const end = start + Math.floor(Math.random() * 256)
+              const start = Math.floor(Math.random() * 128)
+              const end = start + Math.floor(Math.random() * 128)
               this.queue.push({ from, to, start, end })
             }
             cancelAnimationFrame(this.frameRequest)

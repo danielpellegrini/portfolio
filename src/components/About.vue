ee527f5c6a87daefab95b9e1ab33adfb96f9210b
danielpellegrini <danielpellegrini87@gmail.com>
2021-07-08 18:40:04 +0200

    about section completed

--- src/components/About.vue
+++ src/components/About.vue
@@ -9,7 +9,8 @@
             <main id="about">
               <div class="label-wrap">
               </div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odit accusantium id nisi deserunt ex porro itaque. Suscipit eos doloremque minima, repudiandae dolorum autem nobis, modi enim ex, maxime cumque!</p>                
+                <p id="paragraph">
+                </p>                
             </main>
             
               <i id="expandAbout" @click="expandAbout()" class="fas fa-expand-arrows-alt"></i>
@@ -20,6 +21,18 @@
 
 <script>
 export default {
+  mounted() {
+
+    // const par = document.getElementById('paragraph').innerText
+    const par = 'My name is Daniel Pellegrini*In 2020, after 16 years of totally different fields jobs, I decided to change my career* I wanted to learn about what I was really interested in and what I enjoyed doing in my free time and so I began putting serious effort into creating websites* In October 2020, I quit my job and I attended a Full Stack Web Developer course in Boolean Careers*We started with <span class="fab fa-html5"></span> and <span class="fab fa-css3-alt"></span> with <span class="fab fa-sass"></span> as prepocessor and <span class="fab fa-bootstrap"></span> as framework*After several exercises we turned to <span class="fab fa-js-square"></span> and we started to build more dynamic pages with <span class="fab fa-vuejs"></span> framework\'s help*Then we went deeper into programming, so we started learning <span class="fab fa-php"></span> with its framework <span class="fab fa-laravel"></span> and databases management with <span id="fa-mysql"></span>*At the end of the course we had an exam and I developed, with a team of 5 course mates, a complex web app, including Backend (Authentication, integration of a payment service with Braintree APIs and management of charts with Chart-Js) and Frontend with responsive interface*<span class="fas fa-caret-right"> <b></span> Final project GitHub repo link <a href="https://github.com/danielpellegrini/Bdoctors.git" target="blank">here</a></b> <span class="fas fa-caret-left"></span>'
+    const result = par.split('*').join('. <br/>');
+
+    document.getElementById('paragraph').innerHTML = result;
+    const image = document.createElement('img');
+    image.setAttribute('src', "https://cdn.iconscout.com/icon/free/png-512/mysql-19-1174939.png");
+    document.getElementById('fa-mysql').appendChild(image);
+    console.log(result);
+  },
   methods: {
      expandAbout() {
       document.getElementById("expandAboutLayover").className = "max-width holoContainerLayover";

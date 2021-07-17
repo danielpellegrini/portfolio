8f39a213eb9db9e90cef334a0f59cf3d37a477db
danielpellegrini <danielpellegrini87@gmail.com>
2021-07-17 18:48:59 +0200

    vue.config.js updated

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,9 @@
-const SitemapPlugin = require('sitemap-webpack-plugin').default;
-const paths = [
-    {
-        path: '/',
-        lastmod: new Date().toISOString().slice(0,10),
-        priority: '0.8',
-        changefreq: 'hourly'
-    },
-];
-
 module.exports = {
-    configureWebpack: {
-        plugins: [
-            new SitemapPlugin('https://danielpellegrini-webdev.me/', paths, {
-                filename: 'sitemap.xml',
-                lastmod: true,
-                changefreq: 'hourly',
-                priority: '0.8'
-            })
-        ]
+    pluginOptions: {
+        sitemap: {
+            urls: [
+                'https://danielpellegrini-webdev.me'
+            ]
+        }
     }
 }
